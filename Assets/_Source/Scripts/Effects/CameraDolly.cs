d5078f8111433f256e24efab875042e4d78cd4bf
Varez Yeyez <varez.yeyez@gmail.com>
2024-10-23 12:55:28 +0700

    Camera Dolly Main Menu

--- Assets/_Source/Scripts/Effects/CameraDolly.cs
+++ Assets/_Source/Scripts/Effects/CameraDolly.cs
@@ -1,8 +1,16 @@
+using System;
+using Cinemachine;
 using UnityEngine;
 namespace Effects
 {
     public class CameraDolly : MonoBehaviour
     {
-        
+        public CinemachineVirtualCamera camera;
+
+        private void Start()
+        {
+            // camera.
+        }
+
     }
 }
